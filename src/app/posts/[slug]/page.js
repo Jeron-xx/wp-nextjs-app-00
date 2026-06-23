187ea82e669c4bfafbd6fe6dabe61dd55cf5cf99
Jeron <jeronjeron865@gmail.com>
2026-06-23 18:17:06 +0530

    In Posts page, displayed date/time and author name

--- src/app/posts/[slug]/page.js
+++ src/app/posts/[slug]/page.js
@@ -1,20 +1,26 @@
-export async function getPost(slug) {
+async function getPost(slug) {
     const res = await fetch(
-        `http://localhost/wordpress/wp-json/wp/v2/posts?slug=${slug}`,
+        `http://localhost/wordpress/wp-json/wp/v2/posts?_embed&slug=${slug}`,
         {
             next: { revalidate: 60 },
         }
     );
 
     const posts = await res.json();
+    console.log(posts);
   return posts[0];
 }
 
 export default async function PostPage({ params }) {
     const { slug } = await params;
     const post = await getPost(slug);
+    const author = post._embedded?.author?.[0]?.name || "Unknown Author";
+    const date = post.date ? post.date : "Post Not Created";
 
     // const post = await getPost((await params).slug);
+    // console.log(post);
+    console.log(author);
+    console.log(post.date);
 
     if (!post) {
         return <h1>Post not found</h1>;
@@ -23,6 +29,7 @@ export default async function PostPage({ params }) {
     return (
         <main className="p-8">
             <h1 className="text-3xl font-bold mb-4">{post.title.rendered}</h1>
+            <p className="text-sm text-gray-500 mt-2">By {author} • {date}</p>
             <div dangerouslySetInnerHTML={{__html: post.content.rendered,}}/>
         </main>
     );
